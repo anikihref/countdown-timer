a056fb0193418d40310477fa692e1a9b8b0ade85
Anton Stecyura <grandwar39@gmail.com>
2022-10-12 19:24:44 +0300

    add variable of current year

--- src/helpers/findNextDate.ts
+++ src/helpers/findNextDate.ts
@@ -1,6 +1,7 @@
 import { DATES } from "../static/dates";
 
 export function findNextDate(dateName: string): Date {
+  const currentYear = new Date().getFullYear()
 
   function findDateYear(date: Date): Date {
     if (date.getTime() - Date.now() < 0) {
@@ -12,27 +13,27 @@ export function findNextDate(dateName: string): Date {
 
   switch (dateName) {
     case DATES.NEW_YEAR: {
-        return new Date(new Date().getFullYear() + 1, 0, 0)
+        return new Date(currentYear + 1, 0, 0)
     }
 
     case DATES.CHRISTMAS: {
-        return findDateYear(new Date(new Date().getFullYear(), 11, 25))
+        return findDateYear(new Date(currentYear, 11, 25))
     }
 
     case DATES.FALL: {
-        return findDateYear(new Date(new Date().getFullYear(), 8, 0))
+        return findDateYear(new Date(currentYear, 8, 0))
     }
 
     case DATES.WINTER: {
-        return findDateYear(new Date(new Date().getFullYear(), 11, 0))
+        return findDateYear(new Date(currentYear, 11, 0))
     }
 
     case DATES.SPRING: {
-        return findDateYear(new Date(new Date().getFullYear(), 2, 0))
+        return findDateYear(new Date(currentYear, 2, 0))
     }
 
     case DATES.SUMMER: {
-        return findDateYear(new Date(new Date().getFullYear(), 5, 0))
+        return findDateYear(new Date(currentYear, 5, 0))
     }
 
     default: return new Date()
